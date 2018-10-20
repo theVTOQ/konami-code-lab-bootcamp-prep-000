396cf15f16dae56d3e22982051dd87eba806181d
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-20 23:44:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,10 +22,10 @@ function init() {
 function onKeyDownHandler(e) {
   const key = e.key;
 
-  if (key === alphabet[index]) {
+  if (key === codes[index]) {
     index++;
 
-    if (index === alphabet.length) {
+    if (index === codes.length) {
       alert("Hurray!");
 
       index = 0;
