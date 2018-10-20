1978694bbf1d71ff4e2a04d31de5d2e9f4dc924b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-20 22:40:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,16 +16,18 @@ function init() {
   const body = document.querySelector('body');
   let index = 0;
 
-  body.addEventListener('keydown', function(e) {
-    const key = e.key;
-    if(e === codes[index]){
-      index++;
-      if(index === codes.length){
-        alert('Hurray!');
-        index = 0;
-      }
-    }else{
+  body.addEventListener('keydown', keydownHandler(event));
+}
+
+function keydownHandler(e) {
+  const key = e.key;
+  if(e === codes[index]){
+    index++;
+    if(index === codes.length){
+      alert('Hurray!');
       index = 0;
     }
-  })
+  }else{
+    index = 0;
+  }
 }
